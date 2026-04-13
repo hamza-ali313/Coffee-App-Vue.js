1be89e3b667ffc34d44a46460c5b6d3c728d801f
EishaFatima313 <119784649+EishaFatima313@users.noreply.github.com>
2026-04-14 01:43:48 +0500

    added the loader at start

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,54 @@
-<script setup>
-</script>
-
 <template>
-  <router-view />
-  <CartDrawer />
+  <!-- Loader -->
+  <div v-if="isLoading" class="loader-wrapper">
+    <div class="loader"></div>
+  </div>
+  <div v-else>
+    <router-view />
+    <CartDrawer />
+  </div>
 </template>
 
 <script setup>
-import CartDrawer from "./components/layouts/cart/CartDrawer.vue"
+import CartDrawer from "./components/layouts/cart/CartDrawer.vue";
+
+import { ref, onMounted } from "vue";
+
+const isLoading = ref(true);
+
+onMounted(() => {
+  window.addEventListener("load", () => {
+    isLoading.value = false;
+  });
+});
 </script>
+
+<style lang="scss" scoped>
+.loader-wrapper {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: #fff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999;
+}
+
+.loader {
+  width: 50px;
+  height: 50px;
+  border: 4px solid #ddd;
+  border-top: 4px solid #c48a3a;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
